f3fcb8dbdf7e74d718327427139869c530ee0dbf
David Hinkle <drachs@gmail.com>
2020-02-02 14:39:39 -0800

    Turn off steamvr plugin, add server, client, and editor start scripts

---
+++ client.bat
@@ -0,0 +1,4 @@
+echo off
+
+START "" "C:\Program Files\Epic Games\UE_4.17\Engine\Binaries\Win64\UE4Editor.exe" "C:\Users\drach\Documents\Unreal Projects\PuzzlePlatforms\PuzzlePlatforms.uproject" 10.0.0.177 -game -log
+rem START "" "C:\Program Files\Epic Games\UE_4.17\Engine\Binaries\Win64\UE4Editor.exe" "C:\Users\drach\Documents\Unreal Projects\PuzzlePlatforms\PuzzlePlatforms.uproject" 127.0.0.1 -game -log
---
+++ editor.bat
@@ -0,0 +1,1 @@
+START "" "C:\Program Files\Epic Games\UE_4.17\Engine\Binaries\Win64\UE4Editor.exe" "C:\Users\drach\Documents\Unreal Projects\PuzzlePlatforms\PuzzlePlatforms.uproject"
