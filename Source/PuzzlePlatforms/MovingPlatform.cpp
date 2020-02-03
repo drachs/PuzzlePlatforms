a57cffdff8d678f682fe2efaed727b9e75a24755
David Hinkle <drachs@gmail.com>
2020-02-02 22:03:21 -0800

    Platforming Game

---
+++ Source/PuzzlePlatforms/MovingPlatform.cpp
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MovingPlatform.h"
+
+AMovingPlatform::AMovingPlatform()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	SetMobility(EComponentMobility::Movable);
+}
+
+void AMovingPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (HasAuthority()) {
+		SetReplicates(true);
+		SetReplicateMovement(true);
+	}
+
+	StartLocation = GetActorLocation();
+	EndLocation = GetTransform().TransformPosition(TargetLocation);
+}
+
+void AMovingPlatform::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (!HasAuthority()) {
+		return;
+	}
+
+	FVector Target;
+
+	FVector CurrentLocation = GetActorLocation();
+	FVector Direction = EndLocation - StartLocation;
+	
+	Direction.Normalize();
+	CurrentLocation += Direction * Speed*DeltaTime;
+	SetActorLocation(CurrentLocation);
+
+	if ((CurrentLocation - StartLocation).Size() > (EndLocation - StartLocation).Size()) {
+		FVector tmp = EndLocation;
+		EndLocation = StartLocation;
+		StartLocation = tmp;
+	}
+
+}
---
+++ Source/PuzzlePlatforms/MovingPlatform.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/StaticMeshActor.h"
+#include "MovingPlatform.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PUZZLEPLATFORMS_API AMovingPlatform : public AStaticMeshActor
+{
+	GENERATED_BODY()
+
+public:
+	AMovingPlatform();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere)
+	float Speed = 20;
+	
+	/* For some reason this comes in as world coordinates even though in the editor it is relative to the
+	   platform.  Confusing much? */
+	UPROPERTY(EditAnywhere, Meta = (MakeEditWidget = true))
+	FVector TargetLocation;
+
+private:
+	FVector StartLocation;
+	FVector EndLocation;
+	
+};
