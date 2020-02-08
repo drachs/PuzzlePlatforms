79ccdfae9390dd78e5f621e0e5c3f6ff04f1a267
David Hinkle <drachs@gmail.com>
2020-02-07 22:48:04 -0800

    Ready for online play using console commands

--- Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
+++ Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
@@ -17,10 +17,20 @@ void UPuzzlePlatformsGameInstance::Host()
 {
 	if (GEngine)
 		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Green, TEXT("Hosting"));
+
+	UWorld* World = GetWorld();
+	if (!ensure(World != nullptr)) return;
+
+	World->ServerTravel("/Game/ThirdPersonCPP/Maps/ThirdPersonExampleMap?listen");
 }
 
 void UPuzzlePlatformsGameInstance::Join(const FString& Address)
 {
 	if (GEngine)
 		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Green, FString::Printf(TEXT("Joining %s"), *Address));
+
+	APlayerController* PlayerController = GetFirstLocalPlayerController();
+	if (!ensure(PlayerController != nullptr)) return;
+
+	PlayerController->ClientTravel(Address, ETravelType::TRAVEL_Absolute);
 }
--- server.bat
+++
@@ -1,4 +0,0 @@
-echo off
-
-REM Will listen to 7777 by default but will automatically tick the server port up if another client is listening
-START "" "C:\Program Files\Epic Games\UE_4.17\Engine\Binaries\Win64\UE4Editor.exe" "C:\Users\drach\Documents\Unreal Projects\PuzzlePlatforms\PuzzlePlatforms.uproject" /Game/ThirdPersonCPP/Maps/ThirdPersonExampleMap?listen -server -log
